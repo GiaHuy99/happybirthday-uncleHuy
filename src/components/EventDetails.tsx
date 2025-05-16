7111bdb4400d05b31270da7691f6377c994bccda
Mateo <71zaps-memento@icloud.com>
2025-05-15 21:35:41 -0500

    feat: convert address text to a clickable link in EventDetails component for improved user navigation in google maps

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -41,7 +41,14 @@ const EventDetails = () => {
 								<path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
 								<circle cx="12" cy="10" r="3" />
 							</svg>
-							<p>Av. 12 de octubre, Conjunto residencial oro, Itagui</p>
+							<a
+								href="https://www.google.com/maps/search/?api=1&query=Av.+12+de+octubre+Conjunto+residencial+oro+Itagui"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-rose-600 hover:underline"
+							>
+								Av. 12 de octubre, Conjunto residencial oro, Itagui
+							</a>
 						</div>
 					</div>
 				</div>
