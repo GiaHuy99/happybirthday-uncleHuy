50ea2200bfde35a04192ddd4a10ce482113102e2
Mateo <71zaps-memento@icloud.com>
2025-05-15 20:55:48 -0500

    refactor: move image data to a separate file and update Gallery component to import images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,31 +1,6 @@
-const Gallery = () => {
-	const images = [
-		{
-			src: "/01.jpg",
-			caption: "Foto de Salo",
-		},
-		{
-			src: "/02.jpg",
-			caption: "Foto de Salo",
-		},
-		{
-			src: "/03.jpg",
-			caption: "Foto de Salo",
-		},
-		{
-			src: "/04.jpg",
-			caption: "Foto de Salo",
-		},
-		{
-			src: "/05.jpg",
-			caption: "Foto de Salo",
-		},
-		{
-			src: "/06.jpg",
-			caption: "Foto de Salo",
-		},
-	];
+import { images } from "../data/images";
 
+const Gallery = () => {
 	return (
 		<section
 			id="gallery"
---
+++ src/data/images.ts
@@ -0,0 +1,26 @@
+export const images = [
+	{
+		src: "/01.jpg",
+		caption: "Foto de Salo",
+	},
+	{
+		src: "/02.jpg",
+		caption: "Foto de Salo",
+	},
+	{
+		src: "/03.jpg",
+		caption: "Foto de Salo",
+	},
+	{
+		src: "/04.jpg",
+		caption: "Foto de Salo",
+	},
+	{
+		src: "/05.jpg",
+		caption: "Foto de Salo",
+	},
+	{
+		src: "/06.jpg",
+		caption: "Foto de Salo",
+	},
+];
