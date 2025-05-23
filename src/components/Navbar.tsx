d3edeb464932b975a63e150393ca8f3a7a3416ff
Mateo <71zaps-memento@icloud.com>
2025-05-23 17:12:50 -0500

    style: update Navbar component to adjust icon stroke colors for improved visual consistency based on scroll position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,7 +68,7 @@ const Navbar = () => {
 									height="24"
 									viewBox="0 0 24 24"
 									fill="none"
-									stroke={scrolled ? "text-purple-dark" : "white"}
+									stroke="white"
 									strokeWidth="2"
 									strokeLinecap="round"
 									strokeLinejoin="round"
@@ -83,7 +83,7 @@ const Navbar = () => {
 									height="24"
 									viewBox="0 0 24 24"
 									fill="none"
-									stroke={scrolled ? "text-purple-dark" : "white"}
+									stroke={scrolled ? "#C2B2D5" : "white"}
 									strokeWidth="2"
 									strokeLinecap="round"
 									strokeLinejoin="round"
